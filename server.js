f9d21de79fa413f601def4252adb4ada39668a38
Dan True <dan.true@gmail.com>
2012-02-15 15:17:05 -0800

    second commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
+  res.end("Hello world this is dtrue\n");
 });
  
 server.listen(process.env.PORT || 8001);
