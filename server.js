f19e2d0950b0c54f8c2a9a1092bbe9502f808adb
Dan True <dan.true@gmail.com>
2012-02-15 11:36:38 -0800

    date();

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * @author Dan True
+ */
+
+var http = require('http');
+ 
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+ 
+server.listen(process.env.PORT || 8001);
