194ffd894193856a03c5e73c3165ba1936b39451
Dan True <dan.true@gmail.com>
2012-02-15 17:29:52 -0800

    update js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
 var http = require('http');
  
 var server = http.createServer(function (request, response) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello \n");
+  response.writeHead(200, { "Content-Type": "text/plain" })
+  response.end("Hello \n");
 });
  
 server.listen(process.env.PORT || 8001);
