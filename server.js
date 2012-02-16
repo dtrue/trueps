9f6d662f767c2a38544851973ad9654f2fe4bfca
Dan True <dan.true@gmail.com>
2012-02-15 16:36:43 -0800

    Updated server.js and added aptana github connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
  
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world this is dtrue\n");
+  res.end("Hello world this is dtrue...that is righ dtrue \n");
 });
  
 server.listen(process.env.PORT || 8001);
